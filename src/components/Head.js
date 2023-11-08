1dd49a83e158d788e4d5efa7933b2d1f1fa97cf9
Deepak-nishad <deepakkumar62559@gmail.com>
2023-11-09 00:48:40 +0530

    Header created

---
+++ src/components/Head.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 ">
+        <img
+          className="h-8"
+          alt="hamburger"
+          src="https://w7.pngwing.com/pngs/436/707/png-transparent-hamburger-button-computer-icons-menu-menu-food-text-rectangle.png"
+        />
+        <img
+          alt="youtube logo"
+          className="h-8 mx-2"
+          src="https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-1200-80.jpg"
+        />
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full "
+          type="text"
+        />
+        <button className=" border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user icon"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
